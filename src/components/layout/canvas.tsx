4d196a00bb77c327a66a555b64eea4abb91ce6a5
Ricardo Rivera <ricardorivera@Ricardos-MacBook-Pro.local>
2023-06-23 12:09:02 -0700

    added fade to canvas

--- src/components/layout/canvas.tsx
+++ src/components/layout/canvas.tsx
@@ -1,22 +1,30 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { A11yAnnouncer } from "@react-three/a11y";
 import { OrbitControls, Preload, Stats } from "@react-three/drei";
+import useWindowSize from "@/helpers/useWindowSize";
 
 const Controls = () => {
   const control = useRef(null);
   return <OrbitControls enablePan={false} enableZoom={false} enableRotate={false} ref={control} />;
 };
 const CanvasWrapper = ({ children }) => {
+  const [fade, setFade] = useState(false);
+  const size = useWindowSize();
+  useEffect(() => {
+    const time = setTimeout(() => {
+      setFade(true)
+    }, 1500)
+    return () => clearTimeout(time)
+  }, [])
   return (
-    <>
+    <div style={{ opacity: fade ? 1 : 0, transition: '3s', height: size.height ? size.height : '100%', width: size.width ? size.width : '100%' }}>
       <Canvas
         shadows
         // Is this deprecated or typed wrong? Ignoring for now.
         // @ts-ignore
         mode="concurrent"
         style={{
-
           top: 0,
         }}
       >
@@ -25,7 +33,7 @@ const CanvasWrapper = ({ children }) => {
         {children}
       </Canvas>
       <A11yAnnouncer />
-    </>
+    </div>
   );
 };
 
